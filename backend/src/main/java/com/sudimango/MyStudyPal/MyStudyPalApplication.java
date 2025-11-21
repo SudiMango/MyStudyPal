569d70046272523602bb7eff2cb6a6f6bc151f97
SudiMango <sudipto.islam2005@gmail.com>
2025-11-21 12:32:38 -0800

    Initial commit

---
+++ backend/src/main/java/com/sudimango/MyStudyPal/MyStudyPalApplication.java
@@ -0,0 +1,13 @@
+package com.sudimango.MyStudyPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudyPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStudyPalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sudimango/MyStudyPal/MyStudyPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudimango.MyStudyPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudyPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
