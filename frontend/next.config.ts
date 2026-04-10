61368903537a4fd7305234ccd27628baca68a65a
SudiMango <sudipto.islam2005@gmail.com>
2026-04-10 10:21:08 -0700

    Removed nextjs backend proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,13 +4,7 @@ const nextConfig: NextConfig = {
     output: "standalone",
     devIndicators: false,
     async rewrites() {
-        const backendUrl = process.env.BACKEND_URL || "http://127.0.0.1:8080";
-        return [
-            {
-                source: "/api/:path*",
-                destination: `${backendUrl}/:path*`,
-            },
-        ];
+        return [];
     },
 };
 
